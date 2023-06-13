c7951fa07d7a7cc39c9d4c7840e2b37f78ba316b
Rozali-Ab <rozali_ab@outlook.com>
2023-06-13 22:11:36 +0300

    mock временные данные

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,3 +1,41 @@
+import {getRandomInteger, getRandomValue} from '../utils.js';
+import {FILM_COUNT} from '../const.js';
+import {
+  NAME_COUNT, MAX_COMMENTS_ON_FILM, GenreCount, Rating,
+  AgeRating, Runtime, YearsDuration,
+  DaysDuration, names, surnames,
+  titles, posters, genres, emotions,
+  description, comment, countries,
+} from './const.js';
+
+const getDate = () => {
+  const date = new Date();
+
+  date.setFullYear(
+    date.getFullYear() - getRandomInteger(YearsDuration.MIN, YearsDuration.MAX)
+  );
+
+  return date.toISOString();
+};
+
+const generateFilm = () => ({
+  title: getRandomValue(titles),
+  alternativeTitle: getRandomValue(titles),
+  totalRating: getRandomInteger(Rating.MIN, Rating.MAX),
+  poster: getRandomValue(posters),
+  ageRating: getRandomInteger(AgeRating.MIN, AgeRating.MAX),
+  director: `${getRandomValue(names)} ${getRandomValue(surnames)}`,
+  writers: Array.from({length: NAME_COUNT}, () => `${getRandomValue(names)} ${getRandomValue(surnames)}`),
+  actors: Array.from({length: NAME_COUNT}, () => `${getRandomValue(names)} ${getRandomValue(surnames)}`),
+  release: {
+    date: getDate(),
+    releaseCountry: getRandomValue(countries)
+  },
+  runtime: getRandomInteger(Runtime.MIN, Runtime.MAX),
+  genre: Array.from({length: getRandomInteger(GenreCount.MIN, GenreCount.MAX)}, () => getRandomValue(genres)),
+  description
+});
+
 /*
 {
   "id": "0",
